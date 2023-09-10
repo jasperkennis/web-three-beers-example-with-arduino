03bc42cc22ae31ad3eb70f297187608bedc9ae1e
Jasper Kennis <jasper@fanbace.com>
2023-09-10 12:28:51 +0200

    Added the logic to create and share serial connection

--- src/store/app.js
+++ src/store/app.js
@@ -2,3 +2,5 @@ import { atom } from 'jotai';
 
 export const isLoaderRunningAtom = atom(false);
 export const isSceneLoadedAtom = atom(false);
+export const serialPortAtom = atom(null);
+export const potentiometerPositionAtom = atom(null);
